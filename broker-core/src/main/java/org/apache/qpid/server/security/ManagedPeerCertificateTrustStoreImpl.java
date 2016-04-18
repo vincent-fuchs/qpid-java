vincent-fuchs/qpid-java

commit 998bbef7a96358014a51b010cc54d4a0a0deaa9e

QPID-7158: [Java Broker] Address review comments from orudyy@apache.org
git-svn-id: https://svn.apache.org/repos/asf/qpid/java/trunk@1739751 13f79535-47bb-0310-9956-ffa450edef68

broker-core/src/main/java/org/apache/qpid/server/security/ManagedPeerCertificateTrustStoreImpl.java
@@ -26,19 +26,16 @@ import java.security.GeneralSecurityException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.GregorianCalendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TimeZone;
 import java.util.concurrent.Callable;
 
 import javax.net.ssl.TrustManager;
@@ -461,7 +458,7 @@ public class ManagedPeerCertificateTrustStoreImpl
         }
 
         @Override
-        public List<String> getSubjectAltNames()
+        public List<String> AttrgetSubjectAltNames()
         {
             try
             {
@@ -489,15 +486,15 @@ public class ManagedPeerCertificateTrustStoreImpl
         }
 
         @Override
-        public long getValidFrom()
+        public Date getValidFrom()
         {
-            return _x509cert.getNotBefore().getTime();
+            return _x509cert.getNotBefore();
         }
 
         @Override
-        public long getValidUntil()
+        public Date getValidUntil()
         {
-            return _x509cert.getNotAfter().getTime();
+            return _x509cert.getNotAfter();
         }
     }
 }

broker-codegen/src/main/java/org/apache/qpid/server/model/validation/AttributeAnnotationValidator.java
@@ -394,11 +394,6 @@ public class AttributeAnnotationValidator extends AbstractProcessor
             return true;
         }
 
-        if(typeUtils.isSameType(type,elementUtils.getTypeElement("java.util.Date").asType()))
-        {
-            return true;
-        }
-
         if(typeUtils.isSameType(type,elementUtils.getTypeElement("java.security.cert.Certificate").asType()))
         {
             return true;

broker-core/src/main/java/org/apache/qpid/server/model/AbstractConfiguredObject.java
@@ -180,7 +180,7 @@ public abstract class AbstractConfiguredObject<X extends ConfiguredObject<X>> im
     private boolean _openComplete;
     private boolean _openFailed;
     private volatile State _state = State.UNINITIALIZED;
-    private volatile long _lastOpenedTime;
+    private volatile Date _lastOpenedTime;
 
     protected static Map<Class<? extends ConfiguredObject>, ConfiguredObject<?>> parentsMap(ConfiguredObject<?>... parents)
     {
@@ -988,7 +988,7 @@ public abstract class AbstractConfiguredObject<X extends ConfiguredObject<X>> im
                 }
             });
             _openComplete = true;
-            _lastOpenedTime = System.currentTimeMillis();
+            _lastOpenedTime = new Date();
         }
     }
 
@@ -2794,7 +2794,7 @@ public abstract class AbstractConfiguredObject<X extends ConfiguredObject<X>> im
     }
 
     @Override
-    public final long getLastOpenedTime()
+    public final Date getLastOpenedTime()
     {
         return _lastOpenedTime;
     }

broker-core/src/main/java/org/apache/qpid/server/model/ConfiguredObject.java
@@ -127,7 +127,7 @@ public interface ConfiguredObject<X extends ConfiguredObject<X>>
     State getState();
 
     @DerivedAttribute
-    long getLastOpenedTime();
+    Date getLastOpenedTime();
 
     /**
      * Add a listener which will be informed of all changes to this configuration object

broker-core/src/main/java/org/apache/qpid/server/security/CertificateDetails.java
@@ -35,7 +35,7 @@ public interface CertificateDetails extends ManagedAttributeValue
     String getIssuerName();
     String getSubjectName();
     List<String> getSubjectAltNames();
-    long getValidFrom();
-    long getValidUntil();
+    Date getValidFrom();
+    Date getValidUntil();
 
 }

broker-core/src/main/java/org/apache/qpid/server/security/NonJavaKeyStoreImpl.java
@@ -152,13 +152,13 @@ public class NonJavaKeyStoreImpl extends AbstractKeyStore<NonJavaKeyStoreImpl> i
     @Override
     public Date getCertificateValidEnd()
     {
-        return _certificate == null ? new Date(0) : _certificate.getNotAfter();
+        return _certificate == null ? null : _certificate.getNotAfter();
     }
 
     @Override
     public Date getCertificateValidStart()
     {
-        return _certificate == null ? new Date(0) : _certificate.getNotBefore();
+        return _certificate == null ? null : _certificate.getNotBefore();
     }
 
 

commit 28c58e087d766ae1a3ddff61884dace34b885df3

QPID-7158: [Java Broker] Fix typo introduced by r1739751
git-svn-id: https://svn.apache.org/repos/asf/qpid/java/trunk@1739758 13f79535-47bb-0310-9956-ffa450edef68

broker-core/src/main/java/org/apache/qpid/server/security/ManagedPeerCertificateTrustStoreImpl.java
@@ -458,7 +458,7 @@ public class ManagedPeerCertificateTrustStoreImpl
         }
 
         @Override
-        public List<String> AttrgetSubjectAltNames()
+        public List<String> getSubjectAltNames()
         {
             try
             {