vincent-fuchs/qpid-java

commit 9dd43859f49e88d4e76df28e3ea6911e8ed4af09

QPID-7394: [Java Client, AMQP 0-8..0-91] Correct prefetch accounting when message rolled back
git-svn-id: https://svn.apache.org/repos/asf/qpid/java/trunk@1759774 13f79535-47bb-0310-9956-ffa450edef68

client/src/main/java/org/apache/qpid/client/AMQSession_0_8.java
@@ -809,9 +809,10 @@ public class AMQSession_0_8 extends AMQSession<BasicMessageConsumer_0_8, BasicMe
         TxRollbackBody body = getMethodRegistry().createTxRollbackBody();
         AMQFrame frame = body.generateFrame(getChannelId());
         getProtocolHandler().syncWrite(frame, TxRollbackOkBody.class);
+        _currentPrefetch.set(0);
     }
 
-    public void setPrefetchLimits(final int messagePrefetch, final long sizePrefetch)
+    void setPrefetchLimits(final int messagePrefetch, final long sizePrefetch)
             throws QpidException, FailoverException
     {
         _currentPrefetch.set(0);

commit 195b5ae5521264ace2fbbbc7c87630d0c793b49e

QPID-7394: Correct compilation failure introduced by r1759774
git-svn-id: https://svn.apache.org/repos/asf/qpid/java/trunk@1759783 13f79535-47bb-0310-9956-ffa450edef68

client/src/main/java/org/apache/qpid/client/AMQSession_0_8.java
@@ -812,7 +812,7 @@ public class AMQSession_0_8 extends AMQSession<BasicMessageConsumer_0_8, BasicMe
         _currentPrefetch.set(0);
     }
 
-    void setPrefetchLimits(final int messagePrefetch, final long sizePrefetch)
+    public void setPrefetchLimits(final int messagePrefetch, final long sizePrefetch)
             throws QpidException, FailoverException
     {
         _currentPrefetch.set(0);